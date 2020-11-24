5b80d3412056bc8cb7599074050a34e0f6b5b5f1
marchel173 <74483542+marchel173@users.noreply.github.com>
2020-11-24 17:05:25 +0700

    izin suhu

--- script/main.js
+++
@@ -1,6 +0,0 @@
-var menuToggle = document.querySelector("#menu");
-var nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", function() {
-    nav.classList.toggle("slide");
-});
