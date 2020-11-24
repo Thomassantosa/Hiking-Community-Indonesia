1f8a8037e8710093990f80d12afaf381d2984c0d
learngamedevelopment <69357959+learngamedevelopment@users.noreply.github.com>
2020-11-24 21:27:37 +0700

    Update formDaftar.js

--- script/formDaftar.js
+++ script/formDaftar.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     //Melihat Password
     $("#lihatPw").click(function()
     {
@@ -12,6 +13,7 @@ $(document).ready(function()
         }
     });
     
+    //Yang Konfirmasi Password
     $("#lihatPw2").click(function()
     {
         if($(this).is(':checked'))
@@ -23,41 +25,77 @@ $(document).ready(function()
         }
     }); 
     
+    
 
-
-
-
-
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     //Keterangan buat akun berhasil
     $("#submit").click(function()
     {
         let akunSukses = true;
-        let namaSalah = false;
-        let emailSalah = false;
+        let pw2Salah = false;
+
         let nama = document.getElementById("nama").value;
         let email = document.getElementById("email").value;
+        let pw = document.getElementById("pw").value;
+        let pw2 = document.getElementById("pw2").value;
 
-
-        if (nama == "")
+        if (nama.charAt(0) == "")
         {
             akunSukses = false;
-            $("#infoNama").html("Nama Pengguna Tidak Valid");
+            $("#infoNama").html("Nama Pengguna Tidak Boleh Kosong");
+        } else {
+            $("#infoNama").html("");
         }
 
+        //Menentukan Panjang Kata Sandi
+        if (pw.length < 7 )
+        {
+            akunSukses = false;
+            $("#infoPassword").html("Kata Sandi tidak boleh kurang dari 7");
+        } else {
+            $("#infoPassword").html("");
+        }
+
+        //Mengecek Terdapat @
+        if ((email.charAt(email.length - 6) == '.' && email.charAt(email.length - 3) == '.') || email.charAt(email.length - 4) == '.')
+        {
+            $("#infoEmail").html("");
+        } else {
+            $("#infoEmail").html("Masukan Email dengan benar");
+        }
         
-        if (namaSalah)
+
+
+
+
+        //Mengecek Kata Sanda Dengan Konfirmasi Kata Sandi Sama
+        
+        for (let x = 0 ; x < pw.length ; x++)
         {
-            akunSukses = false;
-            $("#infoEmail").html("Email Tidak Valid");
+            if ((pw.charAt(x) != pw2.charAt(x)) || (pw.length != pw2.length))
+            {
+                akunSukses = false;
+                pw2Salah = true;
+            }
         }
+        
+        if (pw2Salah)
+        {
+            $("#infoPassword2").html("Kata Sandi yang dimasukan tidak sama");
+        } else {
+            $("#infoPassword2").html("");
+        }
+        
 
-
-
+        //Jika BErhasil Akun dapat DIbuat
         //Dicek Berhasil tidak nya
         if (akunSukses)
         {
             alert("Akun Berhasil Dibuat");
+            //Jika BErhasil akan kembali ke Halaman Pertama
+            $("form").attr("action", "index.html");
         }
     })
 
